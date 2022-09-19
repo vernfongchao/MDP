8bbc2caefdafa477c78eae69010e2b58e56a53a0
vernfongchao <Vernchao93@gmail.com>
2022-09-19 16:26:00 -0700

    rough draft on landing page

---
+++ react-app/src/components/PortalPage/PortalPage.js
@@ -0,0 +1,27 @@
+
+import {useState} from "react"
+// import Tabs
+
+const PortalPage = () => {
+
+    const [tabs,setTabs] = useState([])
+
+
+
+
+
+    return(
+        <div>
+            
+            {tabs.map(tab => (
+                <Tabs/ >
+            ))}
+
+
+        </div>
+    )
+
+
+}
+
+export default PortalPage
---
+++ react-app/src/components/Tabs/Tabs.js
@@ -0,0 +1,14 @@
+// import Nav Links
+
+
+const Tabs = () => {
+
+    return(<div>
+        {/* <Nav/> */}
+        
+
+
+        </div>)
+}
+
+export default Tabs
