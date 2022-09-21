3a77de16930133dddf9a84d613c52c46ec3165a4
Lucas <lucascostamonteiro@yahoo.com>
2022-09-21 14:53:45 -0700

    login updated

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Staff
 
@@ -23,4 +23,7 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
+    first_name = StringField('first_name', validators=[DataRequired()])
+    last_name = StringField('last_name', validators=[DataRequired()])
+    position = IntegerField('position', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
