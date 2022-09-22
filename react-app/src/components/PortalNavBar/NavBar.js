9470addb3db8580a17a150847de5d2c88f042bc3
vernfongchao <Vernchao93@gmail.com>
2022-09-22 09:56:59 -0700

    highlight logic for side navbard added

--- react-app/src/components/PortalNavBar/NavBar.js
+++ react-app/src/components/PortalNavBar/NavBar.js
@@ -1,52 +1,57 @@
 
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import Reports from '../Reports/ViewReports/Reports';
 import './NavBar.css'
 
-const NavBar = ({tabs,setTabs, tabIndex}) => {
+const NavBar = ({ tabs, setTabs, tabIndex }) => {
 
   const [idx, setIdx] = useState(0)
 
-  useEffect(()=> {
-    if(tabs.length === 1){
+  useEffect(() => {
+    if (tabs.length === 1) {
       setIdx(0)
     }
-    else if(tabs.length > 1){
+    else if (tabs.length > 1) {
       setIdx(1)
     }
-  },[])
+  }, [])
 
 
-  const setIndex = (e,i) => {
+  const setIndex = (e, i) => {
     let tabTitle = [...tabs]
-    if (i === 0) tabTitle[tabIndex].title = "Ann..."
+    if (i === 0) tabTitle[tabIndex].title = "Dash..."
     else if (i === 1) tabTitle[tabIndex].title = "Report"
     else if (i === 2) tabTitle[tabIndex].title = "Patient"
     setTabs(tabTitle)
     setIdx(i)
   }
 
-  return ( 
+  return (
 
 
     <div className="navBar_main_div">
       <div className="navBar_inner_div">
-        <h1 onClick={(e) => setIndex(e, 0)}>
-          Announcement
-        </h1>
-        <h1 onClick={(e) => setIndex(e,1)}>
-          Reports
-        </h1>
-        <h1 onClick={(e) => setIndex(e,2)}>
+        <div className='navbar-header-container'>
+          <h1 className={idx === 0 ? "navbar-header active-navbar" : "navbar-header"} onClick={(e) => setIndex(e, 0)}>
+            Dashboard
+          </h1>
+        </div>
+        <div>
+          <h1 className={idx === 1 ? "navbar-header active-navbar" : "navbar-header"} onClick={(e) => setIndex(e, 1)}>
+            Reports
+          </h1>
+
+        </div>
+        <h1 className={idx === 2 ? "navbar-header active-navbar" : "navbar-header"} onClick={(e) => setIndex(e, 2)}>
           Patient
         </h1>
       </div>
 
 
       <div className="navBar_reports_div">
-        <Reports idx={idx}/>
+        <Reports idx={idx} />
       </div>
     </div>
     // <nav>
--- react-app/src/components/Tabs/Tabs.js
+++ react-app/src/components/Tabs/Tabs.js
@@ -5,7 +5,7 @@ import './Tabs.css'
 const Tabs = () => {
     const [index, setIndex] = useState(0)
 
-    const [tabs, setTabs] = useState([{ title: "Ann..." }])
+    const [tabs, setTabs] = useState([{ title: "Dash... "}])
 
     const addTabs = async () => {
         setTabs([...tabs, { title: "Report" }])
