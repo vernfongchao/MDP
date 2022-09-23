e86f55697c82baebec1709667c15c325c3e80e7c
Code-rina <katerina.kreibich@gmail.com>
2022-09-23 14:27:43 -0700

    css updated for the page

--- react-app/src/components/auth/Login/LoginFormModal.js
+++ react-app/src/components/auth/Login/LoginFormModal.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Modal } from '../../../context/Modal/Modal';
 import LoginForm from './LoginForm';
 
+
 function LoginFormModal() {
   const [showModal, setShowModal] = useState(false);
   return (
