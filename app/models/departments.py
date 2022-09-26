7f09e589e1631447af046a1ab7ceac369caaaae9
vernfongchao <Vernchao93@gmail.com>
2022-09-26 13:44:53 -0700

    merged with main

--- app/models/departments.py
+++ app/models/departments.py
@@ -6,5 +6,5 @@ class Department(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
-    location = db.Column(db.String(100), nullable=False, unique=True)
+    location = db.Column(db.String(100), nullable=False)
 
---
+++ app/seeds/departments.py
@@ -0,0 +1,28 @@
+from app.models import db,Department
+
+
+def seed_deparments():
+    department1 = Department(name="Administration",location="")
+    department2 = Department(name="Human Resources",location="")
+    department3 = Department(name="information technology", location="")
+    department4 = Department(name="Accident and Emergency", location="")
+    department5 = Department(name="Radiology", location="")
+    department6 = Department(name="Cardiology", location="")
+    department7 = Department(name="Surgery", location="")
+    department8 = Department(name="General Services", location="")
+    department9 = Department(name="Intensive Care Unit", location="")
+    department10 = Department(name="Neurology", location="")
+    department11 = Department(name="Gynaecology", location="")
+    department12 = Department(name="Occupational Therapy", location="")
+    department13 = Department(name="Oncology", location="")
+    department14 = Department(name="Orthopaedics", location="")
+    department15 = Department(name="Pharmacy", location="")
+    department16 = Department(name="Adult Clinic", location="")
+    department17 = Department(name="Women Clinic", location="")
+    department18 = Department(name="Pediatrics Clinic", location="")
+    department19 = Department(name="Dental Clinic", location="")
+    department20 = Department(name="Optometry Clinic", location="")
+    department21 = Department(name="Ophthalmology", location="")
+
+
+
