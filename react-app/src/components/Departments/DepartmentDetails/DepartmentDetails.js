64f4e405858c948ffd4b50fa73dda06cb08a2179
Code-rina <katerina.kreibich@gmail.com>
2022-09-26 13:34:47 -0700

    Department css updated with 3 columns

---
+++ react-app/src/components/Departments/DepartmentDetails/DepartmentDetails.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./DepartmentDetails.css"
+
+const DepartmentDetails = () => {
+
+
+    return (
+        <div className="department-details-page-container">
+            <div className="department-detail-header-container">
+                <h1 className="department-information">
+                    Department Information
+                </h1>
+            </div>
+        </div>
+    )
+};
+
+export default DepartmentDetails;
---
+++ react-app/src/components/Departments/DepartmentStaff/DepartmentStaff.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./DepartmentStaff.css"
+
+const DepartmentStaff = () => {
+
+
+    return (
+        <div className="department-staff-page-container">
+            <div className="department-staff-header-container">
+                <h1 className="department-staff-information">
+                    Staff Information
+                </h1>
+            </div>
+        </div>
+    )
+};
+
+export default DepartmentStaff;
