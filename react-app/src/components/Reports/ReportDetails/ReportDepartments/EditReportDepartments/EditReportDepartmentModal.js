5b25934ed78b2b97c76670cecfc3510672aef339
vernfongchao <Vernchao93@gmail.com>
2022-09-30 22:44:22 -0700

    repart departments partially styled except for adding

--- react-app/src/components/Reports/ReportDetails/ReportDepartments/EditReportDepartments/EditReportDepartmentModal.js
+++ react-app/src/components/Reports/ReportDetails/ReportDepartments/EditReportDepartments/EditReportDepartmentModal.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import { Modal } from '../../../../../context/Modal/Modal';
 import EditReportDepartments from './EditReportDepartments.js';
+import * as RiIcons from 'react-icons/ri'
 
 import './EditReportDepartments.css'
 
-const EditReportDepartmentModal = ({ report}) => {
+const EditReportDepartmentModal = ({ report }) => {
     const [showModal, setShowModal] = useState(false);
     return (
-        <>
-            <button onClick={() => setShowModal(true)}>Edit</button>
+        <div className="report-department-edit-modal-container">
+            <RiIcons.RiFileEditFill
+                className='report-department-edit-icon'
+                onClick={() => setShowModal(true)}
+            />
             {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <EditReportDepartments
-                        report={report}/>
+                        report={report}
+                        setShowModal={setShowModal}
+                    />
                 </Modal>
             )}
-        </>
+        </div>
     );
 }
 
--- react-app/src/components/Reports/ReportDetails/ReportDepartments/EditReportDepartments/EditReportDepartments.js
+++ react-app/src/components/Reports/ReportDetails/ReportDepartments/EditReportDepartments/EditReportDepartments.js
@@ -4,8 +4,11 @@ import { useSelector, useDispatch } from "react-redux"
 
 import { patchReportDepartments } from "../../../../../store/departmentreport"
 
+import * as RiIcons from 'react-icons/ri'
+import * as Mdicons from 'react-icons/md'
+import * as VscIcons from 'react-icons/vsc'
 
-const EditReportDepartments = ({ report }) => {
+const EditReportDepartments = ({ report, setShowModal }) => {
     const dispatch = useDispatch()
 
     const reportDepartments = useSelector(state => state.departmentReports.report)
@@ -24,10 +27,6 @@ const EditReportDepartments = ({ report }) => {
         )
     })
 
-    console.log("departmentsObj",departmentsObj)
-    console.log("departmentsToAdd",departmentsToAdd)
-    console.log("departmentsToDelete",departmentsToDelete)
-
     useEffect(() => {
         if (report) {
             setDepartmentsObj(reportDepartments)
@@ -43,10 +42,10 @@ const EditReportDepartments = ({ report }) => {
     const handleAdd = (e, departmentId) => {
         console.log("reached")
         setDepartmentsObj({ ...departmentsObj, [departmentId]: { departmentId } })
-        if(!reportDepartments[departmentId]){
+        if (!reportDepartments[departmentId]) {
             setDepartmentsToAdd({ ...departmentsToAdd, [departmentId]: { departmentId } })
         }
-        if (departmentsToDelete[departmentId]){
+        if (departmentsToDelete[departmentId]) {
             const departments = { ...departmentsToDelete }
             delete departments[departmentId]
             setDepartmentsToDelete(departments)
@@ -54,15 +53,15 @@ const EditReportDepartments = ({ report }) => {
     }
 
 
-    const handleDelete = (e,departmentId, i) => {
-        const newDepartmentsObj = {...departmentsObj}
+    const handleDelete = departmentId => {
+        const newDepartmentsObj = { ...departmentsObj }
         delete newDepartmentsObj[departmentId]
         setDepartmentsObj(newDepartmentsObj)
-        if(reportDepartments[departmentId]){
-            const toBeDelete = { ...departmentsToDelete,[departmentId]:{ departmentId}}
-            setDepartmentsToDelete({...toBeDelete})
+        if (reportDepartments[departmentId]) {
+            const toBeDelete = { ...departmentsToDelete, [departmentId]: { departmentId } }
+            setDepartmentsToDelete({ ...toBeDelete })
         }
-        if(departmentsToAdd[departmentId]){
+        if (departmentsToAdd[departmentId]) {
             const departments = { ...departmentsToAdd }
             delete departments[departmentId]
             setDepartmentsToAdd(departments)
@@ -74,42 +73,68 @@ const EditReportDepartments = ({ report }) => {
         const addDepartmentsLength = Object.values(departmentsToAdd).length
         const newReportDepartments = await dispatch(patchReportDepartments({
             "id": report.id,
-
             [deleteDepartmentsLength ? "delete_departments" : undefined]: deleteDepartmentsLength ? Object.values(departmentsToDelete) : null,
             [addDepartmentsLength ? "add_departments" : undefined]: addDepartmentsLength ? Object.values(departmentsToAdd) : null,
-            // "delete_departments": Object.values(departmentsToDelete),
-            // "add_departments": Object.values(departmentsToAdd)
         }))
+        if (newReportDepartments.length) {
+            setShowModal(false)
+        }
     }
 
     return (
-        <div className="Edit-Report-Department-Page-Container">
-            <button onClick={handleSave}>
-                Save
-            </button>
-            <button>
-                Cancel
-            </button>
-            <h1>
-                Departments
-            </h1>
-            <label>Search</label>
-            <input
-            value={search}
-            onChange={e => setSearch(e.target.value)}
-            />
-            {Object.values(departmentsObj)?.map(({ departmentId }, i) => (
-                <div
-                    key={departmentId}>
-                    <button onClick={e => handleDelete(e,departmentId, i)}>
-                        x
-                    </button>
-                    <span className="report-department-name">
-                        {departments[departmentId].name}
-                    </span>
-                </div>
+        <div className="edit-report-department-page-container">
+            <div className="edit-report-department-icon-container">
 
-            ))}
+                <RiIcons.RiSave3Fill
+                    className='report-edit-icon'
+                    onClick={handleSave}
+                />
+                <Mdicons.MdOutlineClear
+                    className="edit-report-department-cancel-icon"
+                    onClick={e => setShowModal(false)}
+                />
+            </div>
+            <div className="edit-report-department-header-container">
+                <h1>
+                    Departments
+                </h1>
+
+            </div>
+            <div className="edit-report-department-search-container">
+                <div className="edit-report-deparment-search-label-container">
+                    <label
+                        className="edit-report-deparment-search-label"
+                    >Search</label>
+                </div>
+                <div className="edit-report-department-input-container">
+                    <input className="edit-report-department-search-input"
+                        value={search}
+                        onChange={e => setSearch(e.target.value)}
+                    />
+                    <VscIcons.VscSearchStop
+                        className="edit-report-department-search-clear-icon"
+                        onClick={clearSearch}
+                    />
+
+                </div>
+            </div>
+            <div className="edit-report-department-remove-container">
+                {Object.values(departmentsObj)?.map(({ departmentId }, i) => (
+                    <div className="edit-report-department-name-container"
+                        key={departmentId}>
+                        <span className="report-department-name">
+                            {departments[departmentId].name}
+                        </span>
+                        <div className="edit-report-department-remove-icon-container">
+                            <Mdicons.MdOutlineClear
+                                className="edit-report-department-remove-icon"
+                                onClick={e => handleDelete(departmentId)}
+                            />
+                        </div>
+                    </div>
+
+                ))}
+            </div>
 
             {filteredDepartments.map((department) => (
                 <div
