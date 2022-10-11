ff5c044d6fc75f7cb63311621cef0408cae2fcdb
vernfongchao <Vernchao93@gmail.com>
2022-10-11 15:30:37 -0700

    added room seeders

--- app/seeds/rooms.py
+++ app/seeds/rooms.py
@@ -2,7 +2,9 @@ from app.models import db,Room
 
 def seed_rooms():
     rooms = [
-        Room(),
+        Room(staff_id_1=2,staff_id_2=1),
+        Room(staff_id_1=1, staff_id_2=3),
+        Room(staff_id_1=1, staff_id_2=4),
     ]
 
     db.session.add_all(rooms)
