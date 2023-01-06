f65bfc10a0646231e199db3831d98c9f96242b92
vernfongchao <Vernchao93@gmail.com>
2023-01-05 18:48:01 -0800

    bug testing for render

--- app/s3_config.py
+++ app/s3_config.py
@@ -2,6 +2,7 @@ import boto3
 import botocore
 import os
 import uuid
+from gevent import monkey
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
@@ -26,7 +27,9 @@ def get_unique_filename(filename):
 
 
 def upload_file_to_s3(file, acl="public-read"):
-    s3.upload_fileobj(
+
+    try:
+        s3.upload_fileobj(
             file,
             BUCKET_NAME,
             file.filename,
@@ -35,19 +38,9 @@ def upload_file_to_s3(file, acl="public-read"):
                 "ContentType": file.content_type
             }
         )
-    # try:
-    #     s3.upload_fileobj(
-    #         file,
-    #         BUCKET_NAME,
-    #         file.filename,
-    #         ExtraArgs={
-    #             "ACL": acl,
-    #             "ContentType": file.content_type
-    #         }
-    #     )
-    # except Exception as e:
-    #     # in case the our s3 upload fails
-    #     return {"errors": str(e)}
+    except Exception as e:
+        # in case the our s3 upload fails
+        return {"errors": str(e)}
 
     return {"url": f"{S3_LOCATION}{file.filename}"}
 
