6840d7c66247010003cab13bedb20be6408c65ef
vernfongchao <Vernchao93@gmail.com>
2023-01-29 13:44:05 -0800

    pictures added

--- react-app/src/components/Landing/Landing.js
+++ react-app/src/components/Landing/Landing.js
@@ -1,10 +1,15 @@
 import LoginForm from "../auth/Login/LoginForm";
-import "./Landing.css"
+import "./Landing.css";
 
 const LandingPage = () => {
   return (
     <div className="landing">
-      <LoginForm />
+      <h1>
+        Medical Dashboard Portal
+      </h1>
+      <div className="landing__form">
+        <LoginForm />
+      </div>
     </div>
   );
 };
