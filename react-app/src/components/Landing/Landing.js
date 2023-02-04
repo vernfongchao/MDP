14da0c2279501258ff3643d541dec1089c92f584
vernfongchao <Vernchao93@gmail.com>
2023-02-03 19:49:41 -0800

    refractor landing and fix sizing on 1080p

--- react-app/src/components/Landing/Landing.js
+++ react-app/src/components/Landing/Landing.js
@@ -1,45 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import LoginForm from "../auth/Login/LoginForm";
 import Carousel from "./Carousel";
-
-import ImageOne from "./Image/baby.jpg";
-import ImageTwo from "./Image/doctors.jpg";
-import ImageThree from "./Image/operation.jpg";
-import ImageFour from "./Image/heart.jpg";
 
 import "./Landing.css";
 
 const LandingPage = () => {
   const [loading, setLoading] = useState(false);
-  // const [cache, setCache] = useState([]);
-
-  // const cashImages = async (imgArray) => {
-  //   const promises = await imgArray.map((src) => {
-  //     return new Promise(function (resolve, reject) {
-  //       const img = new Image();
-  //       img.src = src;
-  //       img.onload = () => resolve();
-  //       img.onerror = () => reject();
-  //       return img
-  //     });
-  //   });
-
-  //   await Promise.all(promises);
-  //   setCache(promises);
-  //   setLoading(true);
-  // };
-
-  // // useEffect(() => {
-  // //   if(cache.length){
-  // //     setLoading(true)
-  // //   }
-  // // },[cache])
-
-  // // caching images
-  // useEffect(() => {
-  //   const imgs = [ImageOne, ImageTwo, ImageThree, ImageFour];
-  //   cashImages(imgs);
-  // }, []);
 
   return (
     <div className="landing">
